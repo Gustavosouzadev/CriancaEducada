14336484e451e1853a51b786c437ebfb8a0b256d
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-29 01:23:50 -0300

    Finalizando CriancaEducada

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CriancaEducada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Clear();
+            string mensagem;
+
+            Console.Write("Me diga o que você pensa sobre o aluno que não faz a lição de casa: ");
+            mensagem = Console.ReadLine();
+
+            Console.WriteLine("Eu sou uma criança educada. No seu lugar eu diria:");
+            Console.Write(mensagem.Replace("CHATO", "#@$%*!&")
+            .Replace("chato", "#@$%*!&")
+            .Replace("CHATA", "#@$%*!&")
+            .Replace("chata", "#@$%*!&")
+            .Replace("BOBO", "#@$%*!&")
+            .Replace("bobo", "#@$%*!&")
+            .Replace("BOBA", "#@$%*!&")
+            .Replace("boba", "#@$%*!&")
+            .Replace("FEIO", "#@$%*!&")
+            .Replace("feio", "#@$%*!&")
+            .Replace("FEIA", "#@$%*!&")
+            .Replace("feia", "#@$%*!&")
+            .Replace("BOBOCA", "#@$%*!&")
+            .Replace("boboca", "#@$%*!&") 
+            .Replace("BOCÓ", "#@$%*!&")
+            .Replace("bocó", "#@$%*!&") 
+            .Replace("TONTO", "#@$%*!&") 
+            .Replace("tonto", "#@$%*!&") 
+            .Replace("TONTA", "#@$%*!&") 
+            .Replace("tonta", "#@$%*!&")
+            .Replace("PALERMA", "#@$%*!&") 
+            .Replace("palerma", "#@$%*!&") 
+            .Replace("PASPALHO", "#@$%*!&") 
+            .Replace("paspalho", "#@$%*!&") 
+            .Replace("PASPALHA", "#@$%*!&") 
+            .Replace("paspalha", "#@$%*!&") 
+            .Replace("TANTÃ", "#@$%*!&") 
+            .Replace("tantã", "#@$%*!&") 
+            .Replace("PANACA", "#@$%*!&") 
+            .Replace("panaca", "#@$%*!&") 
+            .Replace("PENTELHO", "#@$%*!&") 
+            .Replace("pentelho", "#@$%*!&") 
+            .Replace("PENTELHA", "#@$%*!&") 
+            .Replace("pentelha", "#@$%*!&") 
+            .Replace("BURRO", "#@$%*!&") 
+            .Replace("burro", "#@$%*!&")
+            .Replace("BURRA", "#@$%*!&")
+            .Replace("burra", "#@$%*!&")
+            .Replace("BESTA", "#@$%*!&")
+            .Replace("besta", "#@$%*!&"));
+
+        }
+    }
+}
